c6fa6bc4d3dfa62d982f2ba1962d29ac43dde0fc
Lesly Kinzel <127464833+leslykinzel@users.noreply.github.com>
2025-12-15 16:46:36 +1100

    Submit a query to stratz

--- src/truesight/stratz/__init__.py
+++ src/truesight/stratz/__init__.py
@@ -1,1 +1,22 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+from typing import Any
 
+
+GRAPHQL_URL = "https://api.stratz.com/graphql"
+
+
+class APIClient:
+
+    def __init__(self, token):
+        self._token = token
+
+    def send_query(self, query: str, vars: dict[str, Any]):
+        headers = {
+            "Authorization": f"Bearer {self._token}",
+            "User-Agent": "STRATZ_API",
+        }
+        endpoint = HTTPEndpoint(GRAPHQL_URL, )
+        result = endpoint(query, vars)
+        # just return this for now I will test how
+        # to catch errors later... :)
+        return result
