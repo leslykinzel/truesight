8e87e1210d8c2a235655edf077bf0add0fc8cc15
Lesly Kinzel <127464833+leslykinzel@users.noreply.github.com>
2025-12-20 23:25:52 +1100

    Update __init__.py

--- src/truesight/stratz/__init__.py
+++ src/truesight/stratz/__init__.py
@@ -5,16 +5,18 @@ from typing import Any
 GRAPHQL_URL = "https://api.stratz.com/graphql"
 
 
-class APIClient:
+class StratzAPIClient:
 
-    def __init__(self, token):
+    def __init__(self, token: str):
         self._token = token
 
-    def send_query(self, query: str, vars: dict[str, Any]):
+    def send_query(self, query: str, vars: dict[str, Any]) -> dict[str, Any]:
         headers = {
             "Authorization": f"Bearer {self._token}",
             "User-Agent": "STRATZ_API",
         }
         endpoint = HTTPEndpoint(GRAPHQL_URL, headers)
-        result = endpoint(query, vars)
-        return result
+        response = endpoint(query, vars)
+        if response is None:
+            response = {"data": {}, "headers": {}}
+        return response
